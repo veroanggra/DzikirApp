a23001d6db74fcd544508b33201b9827806593a9
veroanggra <veronicaputri497@gmail.com>
2021-10-14 08:10:30 +0700

    update dzikir

--- app/src/main/java/com/veronica/idn/dzikirapp/MainActivity.kt
+++ app/src/main/java/com/veronica/idn/dzikirapp/MainActivity.kt
@@ -4,17 +4,112 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.veronica.idn.dzikirapp.adapter.ArtikelAdapter
+import com.veronica.idn.dzikirapp.adapter.OnItemClickCallback
+import com.veronica.idn.dzikirapp.databinding.ActivityMainBinding
+import com.veronica.idn.dzikirapp.model.Artikel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    private var artikelArray: ArrayList<Artikel> = arrayListOf()
+    private var dotsCount = 0
+    private lateinit var dotsSlider: Array<ImageView?>
+
+    private val slidingCall = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            for (dot in 0 until dotsCount) {
+                dotsSlider[dot]?.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext, R.drawable.non_active_dot
+                    )
+                )
+            }
+            dotsSlider[position]?.setImageDrawable(
+                ContextCompat.getDrawable(
+                    applicationContext, R.drawable.active_dot
+                )
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        initData()
+        initView()
+        setUpViewPager()
     }
-    companion object{
-        fun getLaunchService(from : Context)=
+
+    private fun setUpViewPager() {
+        val artikelAdapter = ArtikelAdapter(artikelArray)
+        artikelAdapter.setOnItemClickCallback(object : OnItemClickCallback {
+            override fun onItemClicked(data: Artikel) {
+            }
+        })
+
+        mainBinding.vpArtikel.apply {
+            adapter = artikelAdapter
+            registerOnPageChangeCallback(slidingCall)
+        }
+        dotsCount = artikelArray.size
+        dotsSlider = arrayOfNulls(dotsCount)
+        for (i in 0 until dotsCount) {
+            dotsSlider[i] = ImageView(this)
+            dotsSlider[i]?.setImageDrawable(
+                ContextCompat.getDrawable(
+                    applicationContext,
+                    R.drawable.non_active_dot
+                )
+            )
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.setMargins(8, 0, 8, 0)
+            mainBinding.llSlider.addView(dotsSlider[i], params)
+        }
+        dotsSlider[0]?.setImageDrawable(
+            ContextCompat.getDrawable(applicationContext,
+                 R.drawable.active_dot)
+        )
+    }
+
+    private fun initView() {
+        mainBinding.llDzikirDoaShalat.setOnClickListener { }
+        mainBinding.llDzikirDoaHarian.setOnClickListener { }
+        mainBinding.llDzikirSetiapSaat.setOnClickListener { }
+        mainBinding.llDzikirPagiPetang.setOnClickListener { }
+    }
+
+    private fun initData() {
+        val image = resources.obtainTypedArray(R.array.img_artikel)
+        val title = resources.getStringArray(R.array.title_artikel)
+        val desc = resources.getStringArray(R.array.desc_artikel)
+        artikelArray.clear()
+        for (data in title.indices) {
+            artikelArray.add(
+                Artikel(
+                    image.getResourceId(data, 0),
+                    title[data],
+                    desc[data]
+                )
+            )
+        }
+        image.recycle()
+    }
+
+    companion object {
+        fun getLaunchService(from: Context) =
             Intent(from, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                        or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                addFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK
+                            or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                )
             }
     }
 }
--- app/src/main/java/com/veronica/idn/dzikirapp/adapter/ArtikelAdapter.kt
+++ app/src/main/java/com/veronica/idn/dzikirapp/adapter/ArtikelAdapter.kt
@@ -1,4 +1,46 @@
 package com.veronica.idn.dzikirapp.adapter
 
-class ArtikelAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.veronica.idn.dzikirapp.R
+import com.veronica.idn.dzikirapp.databinding.ItemArtikelBinding
+import com.veronica.idn.dzikirapp.model.Artikel
+
+class ArtikelAdapter(private val listArtikel: ArrayList<Artikel>) :
+    RecyclerView.Adapter<ArtikelAdapter.MyViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    inner class MyViewHolder(var itemArtikelBinding: ItemArtikelBinding) :
+        RecyclerView.ViewHolder(itemArtikelBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtikelAdapter.MyViewHolder {
+        val itemArtikelBinding = ItemArtikelBinding
+            .inflate(
+                LayoutInflater
+                    .from(parent.context), parent, false
+            )
+        return MyViewHolder(itemArtikelBinding)
+    }
+
+    override fun onBindViewHolder(holder: ArtikelAdapter.MyViewHolder, position: Int) {
+        holder.itemArtikelBinding.ivArtikel
+            .setImageResource(listArtikel[position].imageArtikel)
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listArtikel[position])
+        }
+    }
+
+    override fun getItemCount(): Int = listArtikel.size
+}
+
+interface OnItemClickCallback {
+    fun onItemClicked(data: Artikel)
 }
